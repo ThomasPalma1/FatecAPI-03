7ce59ec06f5075385591c8ede5cd2515daac569a
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2021-11-25 17:28:39 -0300

    developing the email sending service

--- ionx/src/main/java/com/ionx/ionx/sendEmail/EmailService.java
+++ ionx/src/main/java/com/ionx/ionx/sendEmail/EmailService.java
@@ -6,14 +6,18 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 public class EmailService {
+	
+	private String host = "smtp.gmail.com";
+	private int port = 587;
+	private String ourEmail = "ionx@gmail.com";
+	private String ourPassword = "nossa-senha";
 
-	public JavaMailSenderImpl javaMailSender() {
+	private JavaMailSenderImpl javaMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-		mailSender.setUsername("nosso-email");
-		mailSender.setPassword("nossa-senha");
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+		mailSender.setUsername(ourEmail);
+		mailSender.setPassword(ourPassword);
 
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", true);
@@ -23,14 +27,14 @@ public class EmailService {
 		return mailSender;
 	}
 
-	public void enviarEmailSimples(String titulo, String corpoMensagem, String destinatario) {
-		JavaMailSenderImpl javaMailSender = javaMailSender();
+	public void sendSimpleEmail(String title, String message, String to) {
 		SimpleMailMessage email = new SimpleMailMessage();
-
-		email.setSubject(titulo);
-		email.setText(corpoMensagem);
-		email.setTo(destinatario);
-		email.setFrom("ionx@gmail.com");
+		email.setSubject(title);
+		email.setText(message);
+		email.setTo(to);
+		email.setFrom(ourEmail);
+		
+		JavaMailSenderImpl javaMailSender = javaMailSender();
 		javaMailSender.send(email);
 	}
 }
