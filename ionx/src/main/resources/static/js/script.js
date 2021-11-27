9b909f7aef85a5476d75e0e374c639f3ad0c50dc
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2021-11-27 14:04:58 -0300

    fixing and adding sweet alert when email is sent

--- ionx/src/main/resources/static/js/script.js
+++ ionx/src/main/resources/static/js/script.js
@@ -1,6 +1,6 @@
 /** help */
 function log(message) {
-    console.log('> ' + message)
+	console.log('> ' + message)
 }
 
 /** app */
@@ -10,64 +10,64 @@ const dropzones = document.querySelectorAll('.dropzone')
 
 /** our cards */
 cards.forEach(card => {
-    card.addEventListener('dragstart', dragstart)
-    card.addEventListener('drag', drag)
-    card.addEventListener('dragend', dragend)
+	card.addEventListener('dragstart', dragstart)
+	card.addEventListener('drag', drag)
+	card.addEventListener('dragend', dragend)
 })
 
 function dragstart() {
-    // log('CARD: Start dragging ')
-    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
+	// log('CARD: Start dragging ')
+	dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
 
-    // this = card
-    this.classList.add('is-dragging')
+	// this = card
+	this.classList.add('is-dragging')
 }
 
 function drag() {
-    // log('CARD: Is dragging ')
+	// log('CARD: Is dragging ')
 }
 
 function dragend() {
-    // log('CARD: Stop drag! ')
-    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+	// log('CARD: Stop drag! ')
+	dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
 	sendEmail(this.id)
 
-    // this = card
-    this.classList.remove('is-dragging')
+	// this = card
+	this.classList.remove('is-dragging')
 }
 
 /** place where we will drop cards */
-dropzones.forEach( dropzone => {
-    dropzone.addEventListener('dragenter', dragenter)
-    dropzone.addEventListener('dragover', dragover)
-    dropzone.addEventListener('dragleave', dragleave)
-    dropzone.addEventListener('drop', drop)
+dropzones.forEach(dropzone => {
+	dropzone.addEventListener('dragenter', dragenter)
+	dropzone.addEventListener('dragover', dragover)
+	dropzone.addEventListener('dragleave', dragleave)
+	dropzone.addEventListener('drop', drop)
 })
 
 function dragenter() {
-    // log('DROPZONE: Enter in zone ')
+	// log('DROPZONE: Enter in zone ')
 }
 
 function dragover() {
-    // this = dropzone
-    this.classList.add('over')
+	// this = dropzone
+	this.classList.add('over')
 
-    // get dragging card
-    const cardBeingDragged = document.querySelector('.is-dragging')
+	// get dragging card
+	const cardBeingDragged = document.querySelector('.is-dragging')
 
-    // this = dropzone
-    this.appendChild(cardBeingDragged)
+	// this = dropzone
+	this.appendChild(cardBeingDragged)
 }
 
 function dragleave() {
-    // log('DROPZONE: Leave ')
-    // this = dropzone
-    this.classList.remove('over')
+	// log('DROPZONE: Leave ')
+	// this = dropzone
+	this.classList.remove('over')
 
 }
 
 function drop() {
-    // log('DROPZONE: dropped ')
-    this.classList.remove('over')
+	// log('DROPZONE: dropped ')
+	this.classList.remove('over')
 }
 
