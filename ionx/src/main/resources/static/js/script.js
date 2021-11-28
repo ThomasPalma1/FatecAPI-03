1161404103af642f5da106a9c74cf7bbc32a8c1e
Juliane Freitas <70216694+JulianeFreitass@users.noreply.github.com>
2021-11-28 17:09:15 -0300

    teste kanban

--- ionx/src/main/resources/static/js/script.js
+++ ionx/src/main/resources/static/js/script.js
@@ -13,27 +13,28 @@ function drop(ev) {
 
 var data = ev.dataTransfer.getData("text");
     ev.currentTarget.appendChild(document.getElementById(data));
-    
+
 let dados = ev.currentTarget.id
     console.log("ITEM: " + data + " - COLUNA: " + ev.currentTarget.id);
 console.log(dados)
 
 let id = document.getElementById("idCliente").innerHTML
-	console.log(id)
-	
+    console.log(id)
 
-	
-	//let statusID = document.getElementById('cardStatus').value
-	fetch("http://localhost:8080/edit/" + id,{
-	 method:"PUT",
-	 headers: {"Content-Type": "application/json"},
-	body: JSON.stringify({
-		status: dados,
-		})
-		
-	}).then((resposta) => resposta.json())
-	.then((data) => console.log(data))
-	
-	
-    
+
+
+    //let statusID = document.getElementById('cardStatus').value
+    fetch("http://localhost:8080/edit/" + id,{
+     method:"PUT",
+     headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+        status: dados,
+        })
+
+    }).then((resposta) => resposta.json())
+    .then((data) => console.log(data))
+
+
+
 }
+
