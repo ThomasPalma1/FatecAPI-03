5a625c90bfe69d47f41bfd3791397d64736430ff
Pedro Willian <70727334+pedrowil12@users.noreply.github.com>
2021-11-28 22:29:47 -0300

    Envio de e-mail
    Co-Authored-By: Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
    Co-Authored-By: Juliane Freitas <70216694+JulianeFreitass@users.noreply.github.com>

--- ionx/src/main/java/com/ionx/ionx/repositories/PositionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ionx.ionx.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ionx.ionx.domain.Positions;
-
-public interface PositionsRepository extends JpaRepository <Positions, Long>{
-
-}
